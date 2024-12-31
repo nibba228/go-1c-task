a83d87667f0f77a151e02c54ced6424845f179b1
erestor <raikov.dv@phystech.edu>
2024-12-31 21:58:52 +0300

    init

---
+++ server/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+  "flag"
+  "fmt"
+  "net"
+
+	pb "github.com/nibba228/go-1c-task/experiment"
+	"google.golang.org/grpc/metadata"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+const (
+  guessInProgress = iota
+  guessFinished
+)
+
+type Score struct {
+  guessCount int
+  guessStatus int
+}
+
+type Server struct {
+  scoreTable map[string]Score
+  mutex sync.Mutex
+  start chan struct{}
+}
+
+
+func (s *Server) Register(ctx context.Context, clientInfo *pb.ClientInfo) (*pb.RegisterResponse, error) {
+  _, ok <- s.start
+  if !ok {
+    return &pb.RegisterResponse{
+      Status: "experiment has already started. you cannot join"
+    }, error("experiment started")
+  }
+
+  data, ok := metadata.FromIncomingContext(ctx) 
+
+  if !ok {
+    return nil, error("could not extract context data")
+  }
+
+  mutex.Lock()
+  username, ok = data["username"]
+  mutex.Unlock()
+  if !ok {
+    return nil, error("name is not provided")
+  }
+  mutex.Lock()
+  _, ok = s.scoreTable[username]
+  if ok {
+    defer mutex.Unlock()
+    return &pb.RegisterResponse{
+      Status: "a user with the same name has already registered"
+    }, error("inappropriate username")
+  }
+
+  s.scoreTable[username] = Score{}
+  mutex.Unlock()
+  
+  <-s.start
+  return &RegisterResponse{
+    Status: "starting!",
+  }, nil
+}
+
+func (s *Server) Start(ctx context.Context) (*pb.StartResponse, error) {
+  _, ok <- s.start
+  if !ok {
+    return &pb.StartResponse{
+      Msg: "already started"
+    }, nil
+  }
+
+  close(s.start)
+  return &pb.StartResponse{
+    Msg: "started"
+  }, nil
+}
+
+func newServer() (*Server) {
+  return &Server{
+    scoreTable: make(map[string]Score),
+    mutex: sync.Mutex{},
+    start: make(chan struct{}),
+  }
+}
+
+func main() {
+  flag.Parse()
+  listener, _ := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+  server := grpc.NewServer()
+  pb.RegisterExperimentsServer(server, newServer())
+  server.Serve(listener)
+}
